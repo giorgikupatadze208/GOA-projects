36a673d7bb75126c5d490842bab1985161cc507f
giorgikupatadze208 <kupatadze208@gmail.com>
2024-06-22 15:17:25 +0400

    commit

---
+++ lvl10/homework/.py
@@ -0,0 +1,31 @@
+#hello word
+
+#   N1  hello word daweret 100 jer
+
+for number in range(1,101):
+  print(number)
+  print("hellow word")
+
+#   N2   tanmimdevrobit daweret 1 dan 10 mde
+
+for number in range(1,11):
+  print(number)
+
+#   N3
+
+i=1
+while i<10:
+   print(i)
+   i=i+1
+ 
+
+
+
+
+
+
+
+
+
+
+
