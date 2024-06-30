87449acde04a34400544b7c14749e31b3e31fbbc
giorgikupatadze208 <kupatadze208@gmail.com>
2024-06-30 14:24:19 +0400

    gg

---
+++ lvl13/homework/py.py
@@ -0,0 +1,15 @@
+number = int(input("enter your mark: "))
+
+if 90 <=number<=100:
+   print("your grade is A.")
+elif 80 <= number <= 89:
+   print("your grade is B")
+
+elif 70 <=number <79:
+    print("your grade is C.")
+
+elif 60 <= number <=69:
+    print("yout grade is D.")
+
+else:
+   print("your grade is F.")
