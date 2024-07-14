ea646ab6e0f2eccc328957fb98b1c2ef82297c04
giorgikupatadze208 <kupatadze208@gmail.com>
2024-07-14 14:42:15 +0400

    first commit

--- lvl18/classwork/1.py
+++ lvl18/classwork/1.py
@@ -1,18 +1,7 @@
 from turtle import*
 
+def greeting(name):
+    print(f"hello{name}!")
 
-
-def two_sum(sum1,sum2):
-    print(sum1+sum2)
-
-
-
-first_num=5
-
-secound_num=10
-
-
-
-two_sum(first_num,secound_num)
-
+greeting("giorgi")
 
