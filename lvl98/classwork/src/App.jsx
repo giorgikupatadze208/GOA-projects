309e912113f667d8daaf51623b8cf9eca3601e7c
giorgikupatadze208 <kupatadze208@gmail.com>
2025-04-27 13:06:07 +0400

    first commit

---
+++ lvl98/classwork/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+    const num1 = 5;
+    const num2 = 5;
+    const sum = num1 + num2;
+  
+    return (
+      <div>
+        {num1} + {num2} = {sum}
+      </div>
+    );
+  }
+  
+  export default App;
+  
